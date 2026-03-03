0f4babe3514425879ad14b853d1b01fa4e33ca15
sanyogita <sanyogitaholkar@gmail.com>
2026-03-02 23:24:08 -0800

    First Commit

---
+++ src/test/java/com/example/ad_aggregator/AdAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ad_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
